f06cfa69f03fcb7c20ba9e594962d18c4207c0e1
sarah castorena <s.castorena@outlook.com>
2022-10-11 00:30:14 -0700

    chap7 ex1

--- main.py
+++ main.py
@@ -2,3 +2,15 @@
 # ******************************
 # Make your Code
 # ******************************
+#sarah Castorena
+# SID 2028049
+count =0
+numbers = []
+while count < 5:
+    num = int(input('Enter a number'))
+    numbers.append(num)
+    count = count +1
+
+sum = sum(numbers) - min(numbers) - max(numbers)   
+
+print(sum)
